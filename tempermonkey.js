c84b4d65925f57d04acc604c6f09102fc2b44832
Xav1erSue <s791521574@live.com>
2022-05-17 19:15:11 +0800

    refactor: use es6

--- tempermonkey.js
+++ tempermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【2021】杭电新教务系统自动学评教
 // @namespace    https://github.com/Xav1erSue/Newjw_HDU_AutoChoose
-// @version      1.11
+// @version      2.0
 // @description  杭电新教务系统自动学评教
 // @author       @Xav1erSue
 // @match        http://newjw.hdu.edu.cn/jwglxt/xspjgl/xspj_cxXspjIndex.html*
@@ -11,6 +11,49 @@
 
 (function () {
   ("use strict");
+
+  /* 图形化界面正在开发中…… */
+
+  const config = {
+    /**
+     * 是否开启直接提交
+     * 默认为 false
+     * 开启后点击开始按钮会直接打分并提交
+     */
+    startAndSubmit: false,
+    /**
+     * 选项比例，自动学评教时 A、B、C、D 的分别占比
+     * 默认值为 { A: 0.6, B: 0.4, C: 0, D: 0 } ,即 A: 60%, B: 40%, C: 0%, D: 0%
+     */
+    ratio: {
+      A: 0.6,
+      B: 0.4,
+      C: 0,
+      D: 0,
+    },
+    /**
+     * @todo 待定
+     * 白名单，单独设置老师的学评教参数
+     * （似乎没有什么意义？如果有人希望有这个功能的话我再做吧）
+     */
+    whiteList: [
+      {
+        name: "张三",
+        radio: {
+          A: 0.5,
+          B: 0.3,
+          C: 0.1,
+          D: 0.1,
+        },
+      },
+    ],
+    /**
+     * 监听时间间隔，用于监听 DOM 节点是否已经挂载
+     * 如网络不好可以尝试调大该值
+     * 默认值为 200 ms
+     *  */
+    interval: 200,
+  };
   // 跳过直接进入主页
   $("#btn_yd")
     .prop("disabled", false)
@@ -25,63 +68,75 @@
         { offDetails: "1" }
       );
     });
-  document.getElementById("btn_yd").click();
+  document.querySelector("#btn_yd").click();
 
-  setTimeout(function () {
-    document.getElementsByClassName("ui-pg-selbox")[0].style.outline =
-      "5px solid red";
-  }, 1000);
-
-  // var submit = document.getElementById("btn_xspj_tj");
-  // 为确保不出意外请手动点击提交
-
-  // 挂载开始按钮
-  var head = document.getElementsByClassName("navbar-header")[0];
-  var btn = document.createElement("button");
-  btn.innerText = "点击开始";
-  btn.addEventListener("click", start);
-  head.appendChild(btn);
-  // 提示展开全部课程
-  var notice = document.createElement("span");
-  notice.innerText =
-    "请将下方显示课程数（红框内）调至可以展示所有课程！初始值为15";
-  notice.style.color = "white";
-  head.appendChild(notice);
-  // 单个教师提交
-  function toggleAll() {
-    var radios = document.getElementsByClassName("radio-pjf");
-    for (var i = 0; i < 10; i++) {
-      if (Math.random() >= 0.4) radios[i * 4].checked = true;
-      else radios[i * 4 + 1].checked = true;
+  const timer = setInterval(() => {
+    if (document.querySelector(".ui-pg-selbox")) {
+      document.querySelector(".ui-pg-selbox").style.outline = "5px solid red";
+      const head = document.querySelector(".navbar-header");
+      // 挂载开始评价按钮
+      const startBtn = document.createElement("button");
+      startBtn.innerText = "点击开始";
+      startBtn.addEventListener("click", startAutoChoose);
+      head.appendChild(startBtn);
+      // 提示展开全部课程
+      const notice = document.createElement("span");
+      notice.innerText =
+        "请将下方显示课程数（红框内）调至可以展示所有课程！初始值为15";
+      notice.style.color = "white";
+      head.appendChild(notice);
+      clearInterval(timer);
     }
-    document.getElementById("btn_xspj_bc").click();
-    document.getElementById("btn_ok").click();
-  }
-  // 遍历全部学评教
-  function autoChoose() {
-    // 获取全部可评价课程
-    var saved = parseInt(document.getElementById("bc").children[0].innerText);
-    var lefted = parseInt(document.getElementById("wp").children[0].innerText);
-    var total = saved + lefted;
-    // 从第一门开始
-    document.getElementById(1).click();
-    // 使用计时器循环
-    var i = 1;
-    var timer = setInterval(function () {
-      var cur = document.getElementById(i);
-      if (i >= total) clearInterval(timer);
-      if (cur.children[7].title == "未评") {
-        toggleAll();
+  }, config.interval);
+
+  const toggleAll = (id, limit) => {
+    document.getElementById(id++).click();
+    const timer = setInterval(() => {
+      const radios = document.querySelectorAll(".radio-pjf");
+      if (radios) {
+        for (let i = 0; i < 10; i++) {
+          const rand = Math.random();
+          if (rand <= config.ratio["A"]) radios[i * 4].checked = true;
+          else if (rand <= config.ratio["A"] + config.ratio["B"])
+            radios[i * 4 + 1].checked = true;
+          else if (
+            rand <=
+            config.ratio["A"] + config.ratio["B"] + config.ratio["C"]
+          )
+            radios[i * 4 + 2].checked = true;
+          else radios[i * 4 + 3].checked = true;
+        }
+        clearInterval(timer);
+        // 给 button 添加 enter 属性从而绕过脚本限制
+        if (config.startAndSubmit)
+          $("#btn_xspj_tj").data("enter", "true").click();
+        else $("#btn_xspj_bc").data("enter", "true").click();
+
+        const timer2 = setInterval(() => {
+          if (document.querySelector("#btn_ok")) {
+            clearInterval(timer2);
+            document.querySelector("#btn_ok").click();
+            if (id <= limit) toggleAll(id, limit);
+          }
+        }, config.interval);
       }
-      // 最后再切换视图，留出一秒加载时间
-      i++;
-      document.getElementById(i).click();
-    }, 1000);
-  }
-  function start() {
-    var confirmed = confirm(
-      "您是否已将下方显示课程数（红框内）调至可以展示所有课程\n如果未调整会出现无法全部自动评价的情况！"
-    );
-    if (confirmed) autoChoose();
-  }
+    }, config.interval);
+  };
+
+  const startAutoChoose = () => {
+    const confirmed =
+      confirm(`您是否已将下方显示课程数（红框内）调至可以展示所有课程
+如果未调整会出现无法全部自动评价的情况！
+默认的评价比例为 A: 60%, B: 40%, C: 0%, D: 0%
+默认的提交方式是直接提交
+如果您不希望评价完提交，请到油猴脚本的设置中将 \`startAndSubmit\` 选项设置为 \`false\`
+比例同样可以在油猴脚本的配置中进行手动修改
+图形化修改界面正在开发，后续会增加更多功能！`);
+    if (confirmed) {
+      const saved = parseInt(document.querySelector("#bc > span").innerText);
+      const notRated = parseInt(document.querySelector("#wp > span").innerText);
+      const total = saved + notRated;
+      toggleAll(1, total);
+    }
+  };
 })();
